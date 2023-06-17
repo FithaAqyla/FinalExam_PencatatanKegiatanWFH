444d1ecded64ea7b49d4dfcba3577aa3929196f9
FithaAqyla <fithaqila@gmail.com>
2023-06-17 13:53:45 +0700

    membuat home page karyawan

---
+++ lib/view/home_page_karyawan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomePageK extends StatefulWidget {
+  const HomePageK({super.key});
+
+  @override
+  State<HomePageK> createState() => _HomePageKState();
+}
+
+class _HomePageKState extends State<HomePageK> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

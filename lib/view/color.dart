03b22438efcf31f7779f5c29da865d8c605ed693
FithaAqyla <fithaqila@gmail.com>
2023-06-17 10:05:39 +0700

    menambahkan warna yang akan digunakan

--- lib/view/color.dart
+++ lib/view/color.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
 const Color backgroundss = Color.fromARGB(255, 11, 21, 109);
+const Color iconColor = Color(0xFFB6C7D1);
+const Color activeColor = Color(0xFF09126C);
+const Color textColor1 = Color(0XFFA7BCC7);
+const Color textColor2 = Color(0XFF9BB3C0);
+const Color facebookColor = Color(0xFF3B5999);
+const Color googleColor = Color(0xFFDE4B39);
+const Color backgroundColor = Color(0xFFECF3F9);

3d8e2e7ee5f7e561c0e7f60c6c68f7b9c1c42794
FithaAqyla <fithaqila@gmail.com>
2023-06-23 06:57:51 +0700

    membuat TaskModel

---
+++ lib/model/task_model.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:finalexamflutter/model/task_Item_model.dart';
+import 'package:finalexamflutter/model/user_model.dart';
+
+class TaskModel {
+  final UserModel user;
+  final List<TaskItemModel> tasks;
+  TaskModel({
+    required this.user,
+    required this.tasks,
+  });
+}

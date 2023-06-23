3b3c1ba55b0c1f3b98b949545e3d0debf1242956
FithaAqyla <fithaqila@gmail.com>
2023-06-23 19:56:43 +0700

    membuat task controller

---
+++ lib/controller/task_controller.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:finalexamflutter/model/task_Item_model.dart';
+import 'package:finalexamflutter/model/task_model.dart';
+import 'package:finalexamflutter/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../model/home_model.dart';
+
+class TaskController {
+  final auth = FirebaseAuth.instance;
+  final userCollection = FirebaseFirestore.instance.collection('users');
+  final taskCollection = FirebaseFirestore.instance.collection('tasks');
+
+  TaskModel? data;
+
+  Future<void> initial() async {
+    final user = auth.currentUser;
+    if (user == null) {
+      return;
+    }
+    final userSnapshot = await userCollection.doc(user.uid).get();
+    final userModel = UserModel.fromJson(jsonEncode(userSnapshot.data()));
+    final taskSnapshot = await taskCollection.get();
+    final listTask =
+        taskSnapshot.docs.map((e) => TaskItemModel.fromMap(e.data())).toList();
+    final result = TaskModel(user: userModel, tasks: listTask);
+    data = result;
+  }
+}

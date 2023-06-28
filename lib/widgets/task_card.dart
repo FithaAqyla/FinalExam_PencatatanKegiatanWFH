24416b65b0da32fb5ae900fc630d74088d13c2a6
FithaAqyla <fithaqila@gmail.com>
2023-06-28 21:30:37 +0700

    menambahkan isi taskcard

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,4 +1,6 @@
 import 'package:finalexamflutter/model/task_item_model.dart';
+import 'package:finalexamflutter/view/color.dart';
+import 'package:finalexamflutter/view/task_detail.dart';
 import 'package:finalexamflutter/widgets/status.dart';
 import 'package:flutter/material.dart';
 
@@ -6,49 +8,99 @@ class TaskCard extends StatelessWidget {
   const TaskCard({
     required this.item,
     this.isDeptHead = false,
+    this.maxTitleLines = 1,
+    this.isStatusExpanded = false,
+    this.onBack,
     super.key,
   });
 
   final TaskItemModel item;
   final bool isDeptHead;
+  final bool isStatusExpanded;
+  final int maxTitleLines;
+  final VoidCallback? onBack;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(
-        minHeight: 100,
-        maxHeight: 150,
-      ),
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            item.title,
-            style: const TextStyle(
-              color: Colors.white,
-            ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute<Widget>(
+            builder: (context) {
+              return TaskDetail(
+                isEdit: false,
+                documentId: item.id,
+              );
+            },
           ),
-          const Spacer(),
-          Row(
-            children: [
-              if (isDeptHead)
-                Text(
-                  item.member,
-                  style: const TextStyle(
+        ).then((value) {
+          if (onBack != null) {
+            onBack!.call();
+          }
+        });
+      },
+      child: Container(
+        constraints: const BoxConstraints(
+          maxHeight: 150,
+        ),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: backgroundss,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              item.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: maxTitleLines,
+              overflow: TextOverflow.ellipsis,
+            ),
+            if (maxTitleLines <= 2) ...[
+              const Spacer(),
+            ],
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                if (isDeptHead)
+                  Text(
+                    item.member,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                const Spacer(),
+                Status(
+                  type: StatusType.values.firstWhere(
+                    (element) => element.index == (item.status - 1),
+                  ),
+                  isExpand: isStatusExpanded,
+                ),
+                IconButton(
+                  onPressed: () {
+                    // taskController.deleteContact(id).then(
+                    //   (value) {
+                    //     setState(() {
+                    //       taskController.initial();
+                    //     });
+                    //   },
+                    // );
+                  },
+                  icon: const Icon(
+                    Icons.delete_outline_outlined,
                     color: Colors.white,
                   ),
                 ),
-              const Spacer(),
-              const Status()
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }

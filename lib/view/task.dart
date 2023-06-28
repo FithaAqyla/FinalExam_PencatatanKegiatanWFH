6b7ed2ccde4aa13e37bd79de53ef59fa01cf2561
FithaAqyla <fithaqila@gmail.com>
2023-06-28 21:33:54 +0700

    mengubah isi taskpage

--- lib/view/task.dart
+++ lib/view/task.dart
@@ -1,7 +1,7 @@
 import 'package:finalexamflutter/controller/task_controller.dart';
 import 'package:finalexamflutter/view/color.dart';
 import 'package:finalexamflutter/view/task_detail.dart';
-import 'package:finalexamflutter/widgets/status.dart';
+import 'package:finalexamflutter/widgets/task_card.dart';
 import 'package:flutter/material.dart';
 
 class TaskPage extends StatefulWidget {
@@ -42,69 +42,17 @@ class _TaskPageState extends State<TaskPage> {
             itemCount: taskController.data?.tasks.length ?? 0,
             itemBuilder: (context, index) {
               final item = taskController.data!.tasks[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute<Widget>(
-                      builder: (context) {
-                        return TaskDetail(
-                          isEdit: false,
-                          documentId: item.id,
-                        );
-                      },
-                    ),
-                  );
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  margin: const EdgeInsets.only(bottom: 8),
-                  decoration: BoxDecoration(
-                    color: backgroundss,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        item.title,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          // taskController.deleteContact(id).then(
-                          //   (value) {
-                          //     setState(() {
-                          //       taskController.initial();
-                          //     });
-                          //   },
-                          // );
-                        },
-                        icon: const Icon(
-                          Icons.delete_outline_outlined,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        children: [
-                          if (taskController.data?.user.role == 1) ...[
-                            Text(
-                              item.member,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                          ],
-                          const Spacer(),
-                          const Status(),
-                        ],
-                      ),
-                    ],
-                  ),
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: TaskCard(
+                  item: item,
+                  isDeptHead: taskController.data?.user.role == 1,
+                  maxTitleLines: 3,
+                  isStatusExpanded: true,
+                  onBack: () async {
+                    await taskController.initial();
+                    setState(() {});
+                  },
                 ),
               );
             },
